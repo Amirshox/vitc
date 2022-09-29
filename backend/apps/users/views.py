dfae8d8100ce40cb44c1f869251d50818a648a18
amirshokh <amirshoxmuxitdinov@gmail.com>
2022-09-29 23:32:28 +0500

    serializer and views for contact

---
+++ backend/apps/users/views.py
@@ -0,0 +1,12 @@
+from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin
+from rest_framework.viewsets import ViewSet, ModelViewSet, GenericViewSet
+
+from .models import User
+from .permissions import IsUserOwner
+from .serializers import UserSerializer
+
+
+class UserViewSet(CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, GenericViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = (IsUserOwner,)
